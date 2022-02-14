ca0e788c6a762f7f2890bd4572674a27143c8aa1
wertoto <59776067+wertoto@users.noreply.github.com>
2022-02-14 00:33:12 -0600

    updated firebase contents

--- src/firebase/FireBaseInstance.js
+++ src/firebase/FireBaseInstance.js
@@ -1,6 +1,7 @@
 import {initializeApp} from 'firebase/app';
 import {getAnalytics} from 'firebase/analytics';
-import {collection, addDoc, getDocs, getFirestore} from 'firebase/firestore';
+import {doc, collection, addDoc, getDocs, getFirestore} from 'firebase/firestore';
+import {getAuth, signInWithPopup, GoogleAuthProvider, signInWithRedirect, getRedirectResult} from 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDTGOsZ4zjQGDLwJtRvPloDTcAAtKL5MZ8",
@@ -16,3 +17,69 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore();
 
+const provider = new GoogleAuthProvider();
+
+
+function something() {
+
+}
+
+export default async function signInGooglePopup() {
+    let something = doc(db, 'users/test');
+
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    auth.languageCode = 'it';
+
+    let user = signInWithPopup(auth, provider)
+        .then(
+            (result) => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+
+                const user = result.user;
+
+                return user;
+            }
+        ). catch(
+            (error) => {
+                console.log('error occured');
+                const errCode = error.code;
+                const errMessage = error.message;
+                const email = error.email;
+                const credential = GoogleAuthProvider.credentialFromError(error);
+
+                return '';
+            }
+        );
+
+    await user;
+    console.log('this is user');
+}
+
+export function signInGoogleRedirect() {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithRedirect(auth, provider);
+
+    getRedirectResult(auth)
+        .then(
+            (result) => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+
+                const user = result.user;
+            }
+        ).catch(
+            (error) => {
+                const errCode = error.code;
+                const errMessage = error.message;
+                const email = error.email;
+                const credential = GoogleAuthProvider.credentialFromResult(error);
+            }
+        )
+}
+
+export function saySomething() {
+    console.log('i am gonna say something');
+}
--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,15 +1,30 @@
 import React from "react";
 
+import readData, { saySomething, signInGoogleRedirect } from '../firebase/FireBaseInstance.js';
+import signInGooglePopup from '../firebase/FireBaseInstance.js';
+
 // no design or anything, this is just going to test the firebase sdk
 class TestPage extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            token: ''
+        }
+
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+    }
+
+    handleBtnClick() {
+        let result = signInGooglePopup();
     }
 
     render() {
         return (
             <div>
-                <h1>This page is going to test how Firebase will work with React</h1>
+                <h1>This page is going to test how Firebase will work with JS</h1>
+                <button onClick={this.handleBtnClick}>hihihihihi</button>
+                <h2>{this.state.token}</h2>
             </div>
         )
     }
